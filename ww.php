3067ec27a8fa83d6a5d1bb725c94ec69b1ddac5b
Minh Vu <mbvu@ucsd.edu>
2016-06-17 22:47:54 -0700

    Done with WW, need other sites

--- ww.php
+++ ww.php
@@ -26,6 +26,18 @@ define('MGA_PATH', 'http://www.wuxiaworld.com/mga-index/mga-chapter-');
 define('MGA', 'mga_curr_chapter.txt');
 define('MGA_CURR_CHAP', '788');
 
+//ATG
+define('ATG_PATH', 'http://www.wuxiaworld.com/atg-index/atg-chapter-');
+define('ATG', 'atg_curr_chapter.txt');
+define('ATG_CURR_CHAP', '490');
+
+//TGR
+define('TGR_PATH', 'http://www.wuxiaworld.com/tgr-index/tgr-chapter-');
+define('TGR', 'tgr_curr_chapter.txt');
+define('TGR_CURR_CHAP', '201');
+
+
+
 
 
 
@@ -58,6 +70,7 @@ $html = @file_get_html($saved_path);
 // Check if Page exits
 if ($html == NULL) {
         echo "\n\nISSTH Page Doesnt Exits";
+        fclose($file);
         return;
 }
 
@@ -126,6 +139,7 @@ $html = @file_get_html($saved_path);
 // Check if Page exits
 if ($html == NULL) {
         echo "\n\n DE Page Doesnt Exits";
+        fclose($file);
         return;
 }
 
@@ -193,6 +207,7 @@ $html = @file_get_html($saved_path);
 // Check if Page exits
 if ($html == NULL) {
         echo "\n\n WDQK Page Doesnt Exits";
+        fclose($file);
         return;
 }
 
@@ -260,6 +275,7 @@ $html = @file_get_html($saved_path);
 // Check if Page exits
 if ($html == NULL) {
         echo "\n\n SOTR Page Doesnt Exits";
+        fclose($file);
         return;
 }
 
@@ -327,6 +343,7 @@ $html = @file_get_html($saved_path);
 // Check if Page exits
 if ($html == NULL) {
         echo "\n\n MGA Page Doesnt Exits";
+        fclose($file);
         return;
 }
 
@@ -362,6 +379,143 @@ while ($curr_path != NULL) {
 // Close file
 fclose($file);
 /**   END OF MGA CODE
+ *
+ */
+
+/**
+ * ATG Code Begins
+ */
+
+// open file and see if it exists, if it doesnt create it. If it does then
+// read current chapter from it or set it to XXXX
+$file = @fopen(ATG, 'r');
+if ($file == NULL)
+        $file = fopen(ATG, 'w+');
+
+if (filesize('./' . ATG)) {
+        $curr_chapter = fgets($file);
+}
+else {
+        $curr_chapter = ATG_CURR_CHAP;
+}
+// Close the file and open it so we can write to it
+fclose($file);
+$file = fopen(ATG, 'w');
+
+//Store path to open browser with
+$saved_path = ATG_PATH . $curr_chapter . '/';
+
+// Get HTML
+$html = @file_get_html($saved_path);
+
+// Check if Page exits
+if ($html == NULL) {
+        echo "\n\n ATG Page Doesnt Exits";
+        fclose($file);
+        return;
+}
+
+//Count to not open browser if next chapter Not out
+$count = 0;
+
+// Loops and Get the chapter number for the latest chapter
+$curr_path = $saved_path;
+while ($curr_path != NULL) {
+        $html_check = @file_get_html($curr_path);
+        if ($html_check == NULL) {         
+                if ($count == 4) {
+                        fwrite($file, $curr_chapter - 4);
+                        break;
+                }
+                else {
+                        exec('start ' . $saved_path);
+                        fwrite($file, $curr_chapter - 4);
+                        break;
+                }
+        }
+        foreach ($html_check->find('a') as $a_tag) {
+                if ($a_tag->plaintext == 'Next Chapter') {
+                        $curr_path = $a_tag->href;
+                        $curr_chapter += 1;
+                        $count += 1;
+                        echo $curr_path . PHP_EOL;
+                        break;
+                }
+        }
+}
+
+// Close file
+fclose($file);
+/**   END OF ATG CODE
+ *
+ */
+
+/**
+ * TGR Code Begins
+ */
+
+// open file and see if it exists, if it doesnt create it. If it does then
+// read current chapter from it or set it to XXXX
+$file = @fopen(TGR, 'r');
+if ($file == NULL)
+        $file = fopen(TGR, 'w+');
+
+if (filesize('./' . TGR)) {
+        $curr_chapter = fgets($file);
+}
+else {
+        $curr_chapter = TGR_CURR_CHAP;
+}
+// Close the file and open it so we can write to it
+fclose($file);
+$file = fopen(TGR, 'w');
+
+//Store path to open browser with
+$saved_path = TGR_PATH . $curr_chapter . '/';
+
+// Get HTML
+$html = @file_get_html($saved_path);
+
+// Check if Page exits
+if ($html == NULL) {
+        fwrite($file, $curr_chapter);
+        echo "\n\n TGR Page Doesnt Exits";
+        fclose($file);
+        return;
+}
+
+//Count to not open browser if next chapter Not out
+$count = 0;
+
+// Loops and Get the chapter number for the latest chapter
+$curr_path = $saved_path;
+while ($curr_path != NULL) {
+        $html_check = @file_get_html($curr_path);
+        if ($html_check == NULL) {         
+                if ($count == 1) {
+                        fwrite($file, $curr_chapter);
+                        break;
+                }
+                else {
+                        exec('start ' . $saved_path);
+                        fwrite($file, $curr_chapter);
+                        break;
+                }
+        }
+        foreach ($html_check->find('a') as $a_tag) {
+                if ($a_tag->plaintext == 'Next Chapter') {
+                        $curr_path = $a_tag->href;
+                        $curr_chapter += 1;
+                        $count += 1;
+                        echo $curr_path . PHP_EOL;
+                        break;
+                }
+        }
+}
+
+// Close file
+fclose($file);
+/**   END OF TGR CODE
  *
  */
 
